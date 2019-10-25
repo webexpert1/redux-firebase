543699e9c99f715c8f3e22ec121ab79208816d67
Richard Okoye <emmanuel.eze@netopng.com>
2019-10-25 15:19:27 +0100

    configure firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,18 +2,18 @@ import firebase from 'firebase';
 
   // Your web app's Firebase configuration
   var firebaseConfig = {
-    apiKey: "AIzaSyBtdrAJ6WOHXZmQXLefGvWQ05_fv-smHIs",
-    authDomain: "react-firebase-8c75e.firebaseapp.com",
-    databaseURL: "https://react-firebase-8c75e.firebaseio.com",
-    projectId: "react-firebase-8c75e",
-    storageBucket: "react-firebase-8c75e.appspot.com",
-    messagingSenderId: "778805126632",
-    appId: "1:778805126632:web:6cef77488ae12abb9eb05d",
-    measurementId: "G-758DETS0FG"
+    apiKey: "AIzaSyAWrp5ueHDBxRtjCoxNSJcRGSP5LkLNjPU",
+    authDomain: "redux-hooks-app.firebaseapp.com",
+    databaseURL: "https://redux-hooks-app.firebaseio.com",
+    projectId: "redux-hooks-app",
+    storageBucket: "redux-hooks-app.appspot.com",
+    messagingSenderId: "1018208048510",
+    appId: "1:1018208048510:web:ba6740e0d2dd7b8fc60f2f",
+    measurementId: "G-JQGXS2MPWC"
   };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
-export default firebase;
+export const dbRef = firebase.database();
