361c1e279a119b116bd46bd56f61c647e85934e9
Richard Okoye <emmanuel.eze@netopng.com>
2019-10-25 17:43:33 +0100

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Register from './components/Register';
+import Navigation from './components/Navigation';
 
 
 function App() {
   return(
     <div>
+       <Navigation />
        <Register />
     </div>
   )
---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Layout } from 'antd';
+
+const { Header } = Layout;
+
+const Navigation = () => {
+    return (
+
+    <div>
+        <Layout>
+             <Header></Header>
+        </Layout>
+    </div>
+    )
+}
+
+export default Navigation;
